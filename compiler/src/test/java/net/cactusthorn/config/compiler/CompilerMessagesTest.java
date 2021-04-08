5cd19eddc5b3bf2c4ab17f8c1b04c59b1840d67f
Gmugra <alexei.khatskevich@gmail.com>
2021-04-08 18:47:36 +0200

    #2 : Compiler: Localization support for error messages

---
+++ compiler/src/test/java/net/cactusthorn/config/compiler/CompilerMessagesTest.java
@@ -0,0 +1,24 @@
+package net.cactusthorn.config.compiler;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Locale;
+
+import static net.cactusthorn.config.compiler.CompilerMessages.*;
+import static net.cactusthorn.config.compiler.CompilerMessages.Key.*;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+public class CompilerMessagesTest {
+
+    @BeforeAll
+    static void setup() {
+        Locale.setDefault(Locale.ENGLISH);
+    }
+
+    @Test
+    public void testIt() {
+        assertEquals("Only interface can be annotated with @Config", msg(ONLY_INTERFACE));
+    }
+}
