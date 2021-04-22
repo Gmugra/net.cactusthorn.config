2731858ebef92c1afd0b13aedc03079b81635ca7
Gmugra <alexei.khatskevich@gmail.com>
2021-04-22 20:26:44 +0200

    #26 : API: Loading strategies

--- core/src/main/java/net/cactusthorn/config/core/Config.java
+++ core/src/main/java/net/cactusthorn/config/core/Config.java
@@ -7,4 +7,8 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 @Documented @Target(ElementType.TYPE) @Retention(RetentionPolicy.SOURCE) public @interface Config {
+    enum LoadType {
+       MERGE,
+       FIRST
+    }
 }
