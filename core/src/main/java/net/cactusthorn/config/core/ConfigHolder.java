e7aa8d63e47521a4d9609521c9684f10d818cfc9
Gmugra <alexei.khatskevich@gmail.com>
2021-05-06 18:35:52 +0200

    #78 : API: ConfigHolder get more methods

--- core/src/main/java/net/cactusthorn/config/core/ConfigHolder.java
+++ core/src/main/java/net/cactusthorn/config/core/ConfigHolder.java
@@ -33,36 +33,140 @@ public final class ConfigHolder {
         return get(Function.identity(), key);
     }
 
+    public String getString(String key, String defaultValue) {
+        return get(Function.identity(), key, defaultValue);
+    }
+
+    public Optional<String> getOptionalString(String key) {
+        return getOptional(Function.identity(), key);
+    }
+
     public char getChar(String key) {
         return get(s -> s.charAt(0), key);
+    }
+
+    public char getChar(String key, char defaultValue) {
+        String value = properties.get(key);
+        if (value == null) {
+            return defaultValue;
+        }
+        return value.charAt(0);
+    }
+
+    public Optional<Character> getOptionalChar(String key) {
+        return getOptional(s -> s.charAt(0), key);
     }
 
     public int getInt(String key) {
         return get(Integer::valueOf, key);
     }
 
+    public int getInt(String key, int defaultValue) {
+        String value = properties.get(key);
+        if (value == null) {
+            return defaultValue;
+        }
+        return Integer.valueOf(value);
+    }
+
+    public Optional<Integer> getOptionalInt(String key) {
+        return getOptional(Integer::valueOf, key);
+    }
+
     public byte getByte(String key) {
         return get(Byte::valueOf, key);
+    }
+
+    public byte getByte(String key, byte defaultValue) {
+        String value = properties.get(key);
+        if (value == null) {
+            return defaultValue;
+        }
+        return Byte.valueOf(value);
+    }
+
+    public Optional<Byte> getOptionalByte(String key) {
+        return getOptional(Byte::valueOf, key);
     }
 
     public short getShort(String key) {
         return get(Short::valueOf, key);
     }
 
+    public short getShort(String key, short defaultValue) {
+        String value = properties.get(key);
+        if (value == null) {
+            return defaultValue;
+        }
+        return Short.valueOf(value);
+    }
+
+    public Optional<Short> getOptionalShort(String key) {
+        return getOptional(Short::valueOf, key);
+    }
+
     public long getLong(String key) {
         return get(Long::valueOf, key);
+    }
+
+    public long getLong(String key, long defaultValue) {
+        String value = properties.get(key);
+        if (value == null) {
+            return defaultValue;
+        }
+        return Long.valueOf(value);
+    }
+
+    public Optional<Long> getOptionalLong(String key) {
+        return getOptional(Long::valueOf, key);
     }
 
     public float getFloat(String key) {
         return get(Float::valueOf, key);
     }
 
+    public float getFloat(String key, float defaultValue) {
+        String value = properties.get(key);
+        if (value == null) {
+            return defaultValue;
+        }
+        return Float.valueOf(value);
+    }
+
+    public Optional<Float> getOptionalFloat(String key) {
+        return getOptional(Float::valueOf, key);
+    }
+
     public double getDouble(String key) {
         return get(Double::valueOf, key);
     }
 
+    public double getDouble(String key, double defaultValue) {
+        String value = properties.get(key);
+        if (value == null) {
+            return defaultValue;
+        }
+        return Double.valueOf(value);
+    }
+
+    public Optional<Double> getOptionalDouble(String key) {
+        return getOptional(Double::valueOf, key);
+    }
+
     public boolean getBoolean(String key) {
         return get(Boolean::valueOf, key);
+    }
+
+    public boolean getBoolean(String key, boolean defaultValue) {
+        String value = properties.get(key);
+        if (value == null) {
+            return defaultValue;
+        }
+        return Boolean.valueOf(value);
+    }
+
+    public Optional<Boolean> getOptionalBoolean(String key) {
+        return getOptional(Boolean::valueOf, key);
     }
 
     public <T> T get(Function<String, T> convert, String key) {
