c8c58abc4538cc162640bcc76ed7f8f021e06c25
Gmugra <alexei.khatskevich@gmail.com>
2021-05-26 19:03:13 +0200

    #152 : Avoid code duplication in date-time converters

--- core/src/main/java/net/cactusthorn/config/core/converter/standard/LocalDateConverter.java
+++ core/src/main/java/net/cactusthorn/config/core/converter/standard/LocalDateConverter.java
@@ -20,33 +20,13 @@
 package net.cactusthorn.config.core.converter.standard;
 
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
-import java.time.temporal.ChronoField;
 
-import net.cactusthorn.config.core.converter.Converter;
-
-public class LocalDateConverter implements Converter<LocalDate> {
+public class LocalDateConverter extends DateTimeConverter<LocalDate> {
 
     @Override public LocalDate convert(String value, String[] parameters) {
-        if (parameters == null || parameters.length == 0 || (parameters.length == 1 && "".equals(parameters[0]))) {
+        if (isDefaultFormat(parameters)) {
             return LocalDate.parse(value);
         }
-        DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder();
-        for (String parameter : parameters) {
-            builder.appendPattern('[' + parameter + ']');
-        }
-        // @formatter:off
-        DateTimeFormatter formatter =
-            builder.parseDefaulting(ChronoField.MONTH_OF_YEAR, 1)
-            .parseDefaulting(ChronoField.DAY_OF_MONTH, 1)
-            .parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
-            .parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
-            .parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0)
-            .parseDefaulting(ChronoField.MICRO_OF_SECOND, 0)
-            .parseDefaulting(ChronoField.MILLI_OF_SECOND, 0)
-            .toFormatter();
-        // @formatter:on
-        return LocalDate.parse(value, formatter);
+        return LocalDate.parse(value, createFormatter(parameters));
     }
 }
--- core/src/main/java/net/cactusthorn/config/core/converter/standard/OffsetDateTimeConverter.java
+++ core/src/main/java/net/cactusthorn/config/core/converter/standard/OffsetDateTimeConverter.java
@@ -20,33 +20,13 @@
 package net.cactusthorn.config.core.converter.standard;
 
 import java.time.OffsetDateTime;
-import java.time.format.DateTimeFormatter;
-import java.time.format.DateTimeFormatterBuilder;
-import java.time.temporal.ChronoField;
 
-import net.cactusthorn.config.core.converter.Converter;
-
-public class OffsetDateTimeConverter implements Converter<OffsetDateTime> {
+public class OffsetDateTimeConverter extends DateTimeConverter<OffsetDateTime> {
 
     @Override public OffsetDateTime convert(String value, String[] parameters) {
-        if (parameters == null || parameters.length == 0 || (parameters.length == 1 && "".equals(parameters[0]))) {
+        if (isDefaultFormat(parameters)) {
             return OffsetDateTime.parse(value);
         }
-        DateTimeFormatterBuilder builder = new DateTimeFormatterBuilder();
-        for (String parameter : parameters) {
-            builder.appendPattern('[' + parameter + ']');
-        }
-        // @formatter:off
-        DateTimeFormatter formatter =
-            builder.parseDefaulting(ChronoField.MONTH_OF_YEAR, 1)
-            .parseDefaulting(ChronoField.DAY_OF_MONTH, 1)
-            .parseDefaulting(ChronoField.HOUR_OF_DAY, 0)
-            .parseDefaulting(ChronoField.MINUTE_OF_HOUR, 0)
-            .parseDefaulting(ChronoField.SECOND_OF_MINUTE, 0)
-            .parseDefaulting(ChronoField.MICRO_OF_SECOND, 0)
-            .parseDefaulting(ChronoField.MILLI_OF_SECOND, 0)
-            .toFormatter();
-        // @formatter:on
-        return OffsetDateTime.parse(value, formatter);
+        return OffsetDateTime.parse(value, createFormatter(parameters));
     }
 }
