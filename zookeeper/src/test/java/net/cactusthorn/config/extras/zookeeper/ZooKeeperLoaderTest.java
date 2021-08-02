e54d2159ca12420245037ecc546b2ce44a2381d0
Alexei Khatskevich <alexei.khatskevich@adesso.de>
2021-08-02 08:26:02 +0200

    [extras] ZooKeeper (#204)

--- zookeeper/src/test/java/net/cactusthorn/config/extras/zookeeper/ZooKeeperLoaderTest.java
+++ zookeeper/src/test/java/net/cactusthorn/config/extras/zookeeper/ZooKeeperLoaderTest.java
@@ -1,3 +1,22 @@
+/*
+* Copyright (C) 2021, Alexei Khatskevich
+*
+* Licensed under the BSD 3-Clause license.
+* You may obtain a copy of the License at
+*
+* https://github.com/Gmugra/net.cactusthorn.config/blob/main/LICENSE
+*
+* THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+* AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+* IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+* DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+* FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+* DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+* SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 package net.cactusthorn.config.extras.zookeeper;
 
 import java.util.List;
@@ -10,6 +29,7 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import net.cactusthorn.config.core.factory.ConfigFactory;
 import net.cactusthorn.config.core.loader.Loader;
 
 import org.slf4j.LoggerFactory;
@@ -58,5 +78,14 @@ public class ZooKeeperLoaderTest extends ZooKeeperTestAncestor {
 
         List<ILoggingEvent> logEvents = appender.list;
         assertTrue(logEvents.get(0).getFormattedMessage().startsWith("Can't load resource " + uri));
+    }
+
+    @Test public void fullLoad() {
+        String uri = "zookeeper://";
+        uri += server.getConnectString();
+        uri += BASE_PATH;
+        ZooKeeperConfig config = ConfigFactory.builder().addSource(uri).build().create(ZooKeeperConfig.class);
+        assertEquals(THANKS_VALUE, config.thanks());
+        assertEquals(4, config.greetings().size());
     }
 }
