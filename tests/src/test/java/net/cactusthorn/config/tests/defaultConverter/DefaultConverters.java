c71965132692b5d1d209356ff677b40d6cc0e429
Gmugra <alexei.khatskevich@gmail.com>
2022-01-23 17:56:17 +0100

    [api] add default support for java.util.Locale (#286)

---
+++ tests/src/test/java/net/cactusthorn/config/tests/defaultConverter/DefaultConverters.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2021, Alexei Khatskevich
+ *
+ * Licensed under the BSD 3-Clause license.
+ * You may obtain a copy of the License at
+ *
+ * https://github.com/Gmugra/net.cactusthorn.config/blob/main/LICENSE
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+package net.cactusthorn.config.tests.defaultConverter;
+
+import java.net.URI;
+import java.net.URL;
+import java.nio.file.Path;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.time.OffsetDateTime;
+import java.time.OffsetTime;
+import java.time.Period;
+import java.time.Year;
+import java.time.YearMonth;
+import java.time.ZonedDateTime;
+import java.util.Currency;
+import java.util.Locale;
+
+import net.cactusthorn.config.core.Config;
+import net.cactusthorn.config.core.converter.bytesize.ByteSize;
+
+@Config public interface DefaultConverters {
+
+    Character character();
+
+    URL url();
+
+    URI uri();
+
+    Path path();
+
+    Currency currency();
+
+    Locale locale();
+
+    Instant instant();
+
+    Duration duration();
+
+    Period period();
+
+    LocalDate localDate();
+
+    LocalDateTime localDateTime();
+
+    LocalTime localTime();
+
+    ZonedDateTime zonedDateTime();
+
+    OffsetDateTime offsetDateTime();
+
+    OffsetTime offsetTime();
+
+    Year year();
+
+    YearMonth yearMonth();
+
+    ByteSize byteSize();
+}
